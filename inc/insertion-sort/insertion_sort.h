3887b49ac596ea55cdc0b6fd866655eaba0eb929
hello_chenchen <welcomechenchen@gmail.com>
2019-05-07 21:01:04 +0800

    feature-pre: add insertion sort function

---
+++ inc/insertion-sort/insertion_sort.h
@@ -0,0 +1,30 @@
+/*********************************************************************
+ * cc-algorithm
+ *
+ * Copyright (c) 2019 cc-clipboard contributors:
+ *   - hello_chenchen <https://github.com/hello-chenchen>
+ *
+ * MIT License <https://github.com/hello-chenchen/cc-algorithm/blob/master/LICENSE>
+ * See https://github.com/hello-chenchen/cc-algorithm for the latest update to this file
+ *
+ * author: hello_chenchen <https://github.com/hello-chenchen>
+ **********************************************************************************/
+#ifndef CCLIB_CCALGORITHM_INSERTION_SORT_H
+#define CCLIB_CCALGORITHM_INSERTION_SORT_H
+#include <vector>
+
+using namespace std;
+
+namespace cclib {
+    namespace ccalgorithm {
+        template<typename Comparable>
+        class Sort {
+            public:
+                void insertionSort(vector<Comparable> & sortValue) {
+
+                }
+        };
+    }
+}
+
+#endif   //CCLIB_CCALGORITHM_INSERTION_SORT_H
