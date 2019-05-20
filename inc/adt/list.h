0531a57ac4969714e4bc266658cb6fc5f79a7486
hello_chenchen <welcomechenchen@gmail.com>
2019-05-20 22:01:10 +0800

    feature-pre: test list operator delete function

--- inc/adt/list.h
+++ inc/adt/list.h
@@ -16,18 +16,24 @@ namespace cclib {
     namespace adt {
         template<typename T>
         struct Node {
-            T _data;
-            Node* prev;
-            Node* next;
+            T* _data;
+            Node* _prev;
+            Node* _next;
 
-            Node(): _data = T(), prev(NULL), next(NULL) {}
+            Node(): _data(NULL), _prev(NULL), _next(NULL) {}
+            Node(T* data): _data(data) {}
 
             bool operator==(const Node* instance) const {
-                return (instance->_data == this->_data) && (instance->prev == this->prev) && (instance->next == this->next);
+                return (instance->_data == this->_data) && (instance->_prev == this->_prev) && (instance->_next == this->_next);
             }
 
             bool operator!=(const Node* instance) const {
-                return (instance->_data != this->_data) && (instance->prev != this->prev) && (instance->next != this->next);
+                return (instance->_data != this->_data) && (instance->_prev != this->_prev) && (instance->_next != this->_next);
+            }
+
+            void operator delete(void* node) {
+                delete ((Node<T>*)node)->_data;
+                ((Node<T>*)node)->_prev = ((Node<T>*)node)->_next = NULL;
             }
         };
 
@@ -38,30 +44,40 @@ namespace cclib {
 
             public:
                 ListIterator() {}
-                ListIterator(Node* nodeData) : _M_node(nodeData) {}
-                ListIterator(_Self& itr) : _M_node(itr._M_node) {}
-                ~ListIterator() {}
-                T operator*() { return _M_node->_data; }
-                _Self operator++() {
-                    _M_node = NULL == _M_Node ? NULL : _M_node->next;
-                    return *this;
+
+                ListIterator(Node<T>* nodeData) {
+                    _M_node = nodeData;
                 }
-                _Self operator++(int) {
-                    _Self _temp = *this;
-                    _M_node = NULL == _M_Node ? NULL : _M_node->next;
+
+                ListIterator(const _Self& itr) : _M_node(itr._M_node) {}
+
+                ~ListIterator() {}
+
+                T operator*() { return *(_M_node->_data); }
+
+                _Self operator++() {
+                    _M_node = NULL == _M_node ? NULL : _M_node->_next;
                     return *this;
                 }
 
-                bool operator== (const iterator& instance) const {
+                _Self operator++(int) {
+                    _Self _temp = *this;
+                    _M_node = NULL == _M_node ? NULL : _M_node->_next;
+                    return *this;
+                }
+
+                bool operator== (const _Self& instance) const {
                     return _M_node == instance._M_node;
                 }
 
-                bool operator!= (const iterator& instance) const {
+                bool operator!= (const _Self& instance) const {
                     return _M_node != instance._M_node;
                 }
 
-            private:
-                Node* _M_node;
+            // friend class List;
+
+            public:
+                Node<T>* _M_node;
         };
 
         template <typename T>
@@ -70,42 +86,106 @@ namespace cclib {
                 typedef ListIterator<T> iterator;
 
             public:
-                List() : _size(0), _M_node(NULL) {}
-                List( const List& instance) {
+                explicit List() : _size(0), _M_node(NULL) {
+                    _M_node = new Node<T>();
+                    _M_node->_next = _M_node;
+                    _M_node->_prev = _M_node;
+                }
+
+                List( const List& instance): _size(instance._size) {
                     *this = instance;
                 }
+
                 ~List() {
                     clear();
-                    delete _M_node;
-                    _M_node = NULL;
+
+                    // if(NULL != _M_node) {
+                        // delete _M_node;
+                        // _M_node = NULL;
+                    // }
                 }
 
                 const List& operator=(const List& instance) {
                     if( this != &instance ) {
-                        _M_node
+                        for(iterator itr = instance.begin(); itr != instance.end(); ++itr) {
+                            push_back(*itr);
+                        }
                     }
 
+                    return *this;
                 }
 
-                iterator begin() {}
-                iterator end() {}
+                ListIterator<T> begin() {
+                    return ListIterator<T>((Node<T>*)_M_node->_next);
+                }
 
-                size_t size() const {}
-                bool empty() const {}
-                bool clear() {}
+                iterator end() {
+                    return iterator(_M_node);
+                }
 
-                bool push_front(const T& data) {}
-                bool push_back(const T& data) {}
-                bool pop_front(const T& data) {}
-                bool pop_back(const T& data) {}
+                size_t size() const {
+                    return _size;
+                }
 
-                iterator insert(iterator itr, const T& data) {}
-                iterator erase(iterator itr) {}
-                iterator erase(size_t index) {}
+                bool empty() const {
+                    return _M_node->_next == _M_node;
+                }
+
+                bool clear() {
+                    while(!empty()) {
+                        pop_front();
+                    }
+                    return true;
+                }
+
+                bool push_front(const T& data) {
+                    insert(begin(), data);
+                    return true;
+                }
+
+                bool push_back(const T& data) {
+                    insert(end(), data);
+                    return true;
+                }
+
+                bool pop_front() {
+                    erase(begin());
+                    return true;
+                }
+
+                bool pop_back() {
+                    iterator temp = end();
+                    erase(--temp);  //TODO:
+                }
+
+                iterator insert(iterator itr, const T& data) {
+                    Node<T>* temp = new Node<T>(new T(data));
+                    temp->_next = itr._M_node;
+                    temp->_prev = itr._M_node->_prev;
+                    std::cout << "hello_chenchen: " << std::endl;
+                    itr._M_node->_prev->_next = temp;
+                    itr._M_node->_prev = temp;
+                    _size++;
+
+                    return iterator(temp);
+                }
+
+                ListIterator<T> erase(ListIterator<T> itr) {
+                    Node<T>* nextNode = itr._M_node->_next;
+                    itr._M_node->_prev->_next = itr._M_node->_next;
+                    itr._M_node->_next->_prev = itr._M_node->_prev;
+
+                    // delete itr._M_node->_data;
+                    // itr._M_node->_data = NULL;
+                    delete itr._M_node;
+                    itr._M_node = NULL;
+
+                    return iterator(nextNode);
+                }
 
             private:
                 size_t _size;
-                Node* _M_node;
+                Node<T>* _M_node;
         };
     }
 }
---
+++ test/list_test.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "./../inc/adt/list.h"
+
+using namespace std;
+using namespace cclib;
+using namespace cclib::adt;
+
+void listTest() {
+    List<int> cc;
+    cc.push_back(1);
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    listTest();
+    return 0;
+}
