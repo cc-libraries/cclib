53293e2b6e2ccc91c69a3bd90aaab51945798f93
hello_chenchen <welcomechenchen@gmail.com>
2019-06-01 22:52:23 +0800

    function: map not delete struct

--- test/std_test.cc
+++ test/std_test.cc
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+struct Demo{
+    int a;
+    int b;
+    Demo(int aa, int bb):a(aa), b(bb) {}
+    ~Demo() {
+        std::cout << "~Demo()" << std::endl;
+    }
+};
+
 void foo() {
     list<int> cc;
     cc.push_back(122);
@@ -22,13 +31,18 @@ void foo() {
 }
 
 void foo1() {
-    std::map<int,int> example = {{1,11},{2,12}};
+    std::map<int,Demo*> example;
+    // std::map<int, char> example;
+    // Demo* a1 = new Demo(31, 32);
     cout << "map size: " <<example.size() << endl;
-    example.insert(std::pair<int,char>(3, 12));
+    example.insert(std::pair<int,Demo*>(3, new Demo(31, 32)));
     cout << "map size1: " <<example.size() << endl;
     auto search = example.find(3);
     if(search != example.end()) {
         std::cout << "Found " << search->first << " " << search->second << '\n';
+        // search->second;
+        // Demo b1 = (Demo)search->second;
+        // std::cout << "Demo: " << b1.a << " " << b1.b << endl;
     }
     else {
         std::cout << "Not found\n";
@@ -43,6 +57,7 @@ void foo1() {
     }
 
     example.erase(1);
+
     cout << "map size2: " <<example.size() << endl;
 }
 
